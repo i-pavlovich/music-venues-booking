9f1e5c7937f7fc84adec08648db2a67579c9814a
Ivan Pavlovich <ivan.shemer.pav@gmail.com>
2024-01-09 23:35:59 +0300

    feat: add booking models

--- src/booking/models.py
+++ src/booking/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Service(models.Model):
+    name = models.CharField(max_length=150)
+
+
+class MusicVenue(models.Model):
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=2000)
+    address = models.CharField(max_length=150)
+    services = models.ManyToManyField(Service, verbose_name="available services")
+
+
+class Booking(models.Model):
+    music_venue = models.ForeignKey(MusicVenue, on_delete=models.PROTECT)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
+    username = models.CharField(max_length=100)
