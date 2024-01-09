986c333162d73ec50ab00a936447e2057a9b7df9
Ivan Pavlovich <ivan.shemer.pav@gmail.com>
2024-01-09 23:50:57 +0300

    feat: add str methods for models

--- src/booking/models.py
+++ src/booking/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Service(models.Model):
     name = models.CharField(max_length=150)
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class MusicVenue(models.Model):
     name = models.CharField(max_length=150)
@@ -11,9 +14,15 @@ class MusicVenue(models.Model):
     address = models.CharField(max_length=150)
     services = models.ManyToManyField(Service, verbose_name="available services")
 
+    def __str__(self) -> str:
+        return self.name
+
 
 class Booking(models.Model):
     music_venue = models.ForeignKey(MusicVenue, on_delete=models.PROTECT)
     check_in = models.DateTimeField()
     check_out = models.DateTimeField()
     username = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return f"{self.music_venue.name} music venue is booked by user {self.username}"
