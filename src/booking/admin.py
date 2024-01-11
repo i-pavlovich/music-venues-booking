1ec3e628dde4cd31d01b63ebcf4d52aae8f017fb
Ivan Pavlovich <ivan.shemer.pav@gmail.com>
2024-01-11 23:17:54 +0300

    feat: register models

--- src/booking/admin.py
+++ src/booking/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Booking, MusicVenue, Service
 
-# Register your models here.
+
+admin.site.register(Booking)
+admin.site.register(MusicVenue)
+admin.site.register(Service)
