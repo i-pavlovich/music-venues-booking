625fc83689d731dbd0e864bf349bb9e533865c59
Ivan Pavlovich <ivan.pavlovich.business@gmail.com>
2024-02-23 23:30:35 +0300

    chore: update booking api urls

--- src/booking/urls.py
+++ src/booking/urls.py
@@ -11,10 +11,12 @@ from .views import (
 
 
 urlpatterns = [
-    path("booking/", BookingList.as_view()),
-    path("booking/<int:pk>", BookingDetail.as_view()),
-    path("music_venues/", MusicVenueList.as_view()),
-    path("music_venue/<int:pk>/", MusicVenueDetail.as_view()),
-    path("services/", ServiceList.as_view()),
-    path("service/<int:pk>", ServiceDetail.as_view()),
+    path("booking_list/", BookingList.as_view(), name="booking_list"),
+    path("booking/<int:pk>", BookingDetail.as_view(), name="booking_detail"),
+    path("music_venue_list/", MusicVenueList.as_view(), name="music_venue_list"),
+    path(
+        "music_venue/<int:pk>/", MusicVenueDetail.as_view(), name="music_venue_detail"
+    ),
+    path("service_list/", ServiceList.as_view(), name="service_list"),
+    path("service/<int:pk>", ServiceDetail.as_view(), name="service_detail"),
 ]
