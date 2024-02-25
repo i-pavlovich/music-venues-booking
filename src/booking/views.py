18ab82b39a78f15e192641a20e9d2eb4dba9f457
Ivan Pavlovich <ivan.pavlovich.business@gmail.com>
2024-02-25 23:38:16 +0300

    feat: add filtering and search

--- src/booking/views.py
+++ src/booking/views.py
@@ -1,5 +1,8 @@
+from datetime import datetime
+
 from rest_framework import generics, status
 from rest_framework.exceptions import APIException
+from rest_framework.filters import SearchFilter
 from rest_framework.response import Response
 
 from .models import Booking, MusicVenue, Service
@@ -7,45 +10,9 @@ from .serializers import BookingSerializer, MusicVenueSerializer, ServiceSeriali
 from .services import is_date_available
 
 
-# TODO: Оптимизация запросов
-class MusicVenueDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = MusicVenueSerializer
-    queryset = MusicVenue.objects.all()
-
-
-class MusicVenueList(generics.ListCreateAPIView):
-    serializer_class = MusicVenueSerializer
-
-    def get_queryset(self):
-        queryset = MusicVenue.objects.all().select_related()
-        name = self.request.query_params.get("name")
-        address = self.request.query_params.get("address")
-        if name is not None:
-            queryset = queryset.filter(name=name)
-        if address is not None:
-            queryset = queryset.filter(address=address)
-        return queryset
-
-
-class ServiceDetail(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = ServiceSerializer
-    queryset = Service.objects.all()
-
-
-class ServiceList(generics.ListCreateAPIView):
-    serializer_class = ServiceSerializer
-
-    def get_queryset(self):
-        queryset = Service.objects.all()
-        name = self.request.query_params.get("name")
-        if name is not None:
-            queryset = queryset.filter(name=name)
-        return queryset
-
-
 class BookingDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = BookingSerializer
-    queryset = Booking.objects.all()
+    queryset = Booking.objects.all().select_related("music_venue")
 
 
 class BookingList(generics.ListCreateAPIView):
@@ -54,8 +21,14 @@ class BookingList(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = Booking.objects.all().select_related("music_venue")
         username = self.request.query_params.get("username")
+        history = self.request.query_params.get("history")
         if username is not None:
             queryset = queryset.filter(username=username)
+        if history is not None:
+            if history:
+                queryset = queryset.filter(check_out__lt=datetime.utcnow())
+            else:
+                queryset = queryset.filter(check_out__gte=datetime.utcnow())
         return queryset
 
     def create(self, request, *args, **kwargs):
@@ -73,3 +46,43 @@ class BookingList(generics.ListCreateAPIView):
             detail="The selected date is already taken",
             code=status.HTTP_400_BAD_REQUEST,
         )
+
+
+class MusicVenueDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = MusicVenueSerializer
+    queryset = MusicVenue.objects.all().prefetch_related("services")
+
+
+class MusicVenueList(generics.ListCreateAPIView):
+    serializer_class = MusicVenueSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ["name", "address", "services__name"]
+
+    def get_queryset(self):
+        queryset = MusicVenue.objects.all().prefetch_related("services")
+        name = self.request.query_params.get("name")
+        address = self.request.query_params.get("address")
+        services = self.request.query_params.get("services")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        if address is not None:
+            queryset = queryset.filter(address__icontains=address)
+        if services is not None:
+            queryset = queryset.filter(services__name__icontains=services)
+        return queryset
+
+
+class ServiceDetail(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ServiceSerializer
+    queryset = Service.objects.all()
+
+
+class ServiceList(generics.ListCreateAPIView):
+    serializer_class = ServiceSerializer
+
+    def get_queryset(self):
+        queryset = Service.objects.all()
+        name = self.request.query_params.get("name")
+        if name is not None:
+            queryset = queryset.filter(name__icontains=name)
+        return queryset
